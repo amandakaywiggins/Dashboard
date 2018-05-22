b87a20fd427c689ac0933751d8ec84aa3aeea67d
Amanda Kay <amandakaywiggins@gmail.com>
2018-05-21 21:37:56 -0500

    Giphy and Weather

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 var userName = "";
-var userZip = "";
+var userFavAnimal = "";
 var userBirthday = "";
 
 $("#submit").on("click", function(event){
@@ -115,11 +115,9 @@ if(navigator.geolocation) {
         new google.maps.Geocoder().geocode(
             {'latLng': point},
             function (res, status) {
-                var zip = res[1].address_components[7].long_name;
-                console.log(zip);
                 var APIKey = "0cdaef666666e73cec0a1f220c106a82";
                 var queryURL;
-                var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=" + APIKey;
+                var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=imperial&appid=" + APIKey;
                 console.log(queryURL);
                 $.ajax({
                     url: queryURL,
@@ -137,77 +135,80 @@ if(navigator.geolocation) {
                     console.log("Wind Speed: " + response.wind.speed);
                     console.log("Humidity: " + response.main.humidity);
                     console.log("Temperature (F): " + response.main.temp);
+                    var weatherCode = response.weather[0].id
+                    console.log(weatherCode);
                     //console.log("Rain" +  response.list.rain);
-                    function weatherBackground() {
-                        if (response.weather[0].id === 200 || 230 || 231 || 300 || 301 || 310 || 313 || 321 || 500 || 520) {
+                        if (weatherCode === 200 || 230 || 231 || 300 || 301 || 310 || 313 || 321 || 500 || 520) {
+                            console.log(`weather code is ${weatherCode} so it should be raining`);
                             var lightRainImage = $("<img>")
                             lightRainImage.attr("src" , "assets/images/weather/light_rain.jpg");
                             $("#weather-api").append(lightRainImage);
-                        } else if (response.weather[0].id === 210 || 302 || 311 || 501 || 522 || 531) {
+                        } else if (weatherCode === 210 || 302 || 311 || 501 || 522 || 531) {
                             var rainImage = $("<img>")
                             rainImage.attr("src", "assets/images/weather/rain.jpg");
                             $("#weather-api").append(rainImage);
-                        } else if (response.weather[0].id === 312 || 313 || 314 || 503 || 502) {
+                        } else if (weatherCode === 312 || 313 || 314 || 503 || 502) {
                             var heavyRainImage = $("<img>");
                             heavyRainImage.attr("src", "assets/images/weather/heavy_rain.jpg");
                             $("#weather-api").append(heavyRainImage);
-                        } else if (response.weather[0].id === 202 || 211 || 212 || 221 || 232 || 711) {
+                        } else if (weatherCode === 202 || 211 || 212 || 221 || 232 || 711) {
                             var thunderstormImage = $("<img>");
                             thunderstormImage.attr("src", "assets/images/weather/thunderstorm.jpg");
                             $("#weather-api").append(thunderstormImage);
-                        } else if (response.weather[0].id === 504) {
+                        } else if (weatherCode === 504) {
                             var extremeRainImage = $("<ing>");
                             extremeRainImage.attr("src", "assets/images/weather/extreme_rain.jpg");
                             $("#weather-api").append(extremeRainImage);
-                        } else if (response.weather[0].id === 600 || 615) {
+                        } else if (weatherCode === 600 || 615) {
                             var lightSnowImage = $("<img>");
                             lightSnowImage.attr("src", "assets/images/weather/light_snow.jpg");
                             $("#weather-api").append(lightSnowImage);
-                        } else if (response.weather[0].id === 601 || 616 || 621) {
+                        } else if (weatherCode === 601 || 616 || 621) {
                             var snowImage = $("<img>");
                             snowImage.attr("src", "assets/images/weather/snow.jpg");
                             $("#weather-api").append(snowImage);
-                        } else if (response.weather[0].id === 602, 622) {
+                        } else if (weatherCode === 602, 622) {
                             var heavySnowImage = $("<ing>");
                             heavySnowImage.attr("src", "assets/images/weather/heavy_snow.jpg");
                             $("#weather-api").append(heavySnowImage);
-                        } else if (response.weather[0].id === 611 || 612 || 511) {
+                        } else if (weatherCode === 611 || 612 || 511) {
                             var sleetImage = $("<img>");
                             sleetImage.attr("src", "assets/images/weather/sleet.jpg");
                             $("#weather-api").append(sleetImage);
-                        } else if (response.weather[0].id === 701 || 741) {
+                        } else if (weatherCode === 701 || 741) {
                             var fogImage = $("<img>");
                             fogImage.attr("src", "assets/images/weather/fog.jpg");
                             $("#weather-api").append(fogImage);
-                        } else if (response.weather[0].id === 711) {
+                        } else if (weatherCode === 711) {
                             var smokeImage = $("<img>");
                             smokeImage.attr("src", "assets/images/weather/smoke.jpg");
                             $("#weather-api").append(smokeImage);
-                        } else if (response.weather[0].id === 721) {
+                        } else if (weatherCode === 721) {
                             var hazeImage = $("<img>");
                             hazeImage.attr("src", "assets/images/weather/haze.jpg");
                             $("#weather-api").append(hazeImage);
-                        } else if (response.weather[0].id === 731 || 751 || 761) {
+                        } else if (weatherCode === 731 || 751 || 761) {
                             var duststormImage = $("<img>");
                             duststormImage.attr("src", "assets/images/weather/dust.jpg");
                             $("#weather-api").append(duststormImage);
-                        } else if (response.weather[0].id === 762) {
+                        } else if (weatherCode === 762) {
                             var volcanicAshImage = $("<img>");
                             volcanicAshImage.attr("src", "assets/images/weather/volcanic_ash.jpg");
                             $("#weather-api").append(volcanicAshImage);
-                        } else if (response.weather[0].id === 781) {
+                        } else if (weatherCode === 781) {
                             var tornadoImage = $("<img>");
                             tornadoImage.attr("src", "assets/images/weather/tornado.jpg");
                             $("#weather-api").append(tornadoImage);
-                        } else if (response.weather[0].id === 800) {
+                        } else if (weatherCode === 800) {
+                            console.log("Hi!");
                             var clearImage = $("<img>");
                             clearImage.attr("src", "assets/images/weather/clear.jpg");
                             $("#weather-api").append(clearImage);
-                        } else if (response.weather[0].id === 801, 802) {
+                        } else if (weatherCode === 801, 802) {
                             var scatteredCloudsImage = $("<img>");
                             scatteredCloudsImage.attr("src", "assets/images/weather/scattered_clouds.jpg");
                             $("#weather-api").append(scatteredCloudsImage);
-                        } else if (response.weather[0].id === 803 || 804) {
+                        } else if (weatherCode === 803 || 804) {
                             var overcastImage = $("<img>");
                             overcastImage.attr("src", "assets/images/weather/overcast.jpg");
                             $("#weather-api").append(overcastImage);
@@ -216,11 +217,55 @@ if(navigator.geolocation) {
                             earthImage.attr("src", "assets/images/weather/earth.jpg");
                             $("#weather-api").append(earthImage);
                         };
-                    };
-                weatherBackground();
                 });    
             });
         }
     );
 };
 
+
+//GIPHY API
+function displayCuteAnimals() {
+    database.ref().on("child_added" , function(childSnapshot) {
+        console.log(childSnapshot.val());
+        animal = childSnapshot.val().userFavAnimal;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=1";
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+                    var animalDiv = $("<div>");
+                    animalDiv.addClass("image-style");
+                    var animalImage = $("<img>");
+                    var imageURL = results[i].images.fixed_height.url
+                    var stillImageURL = results[i].images.fixed_height_still.url;
+                    animalImage.attr("src", stillImageURL);
+                    animalImage.attr("data-still" , stillImageURL);
+                    animalImage.attr("data-animate" , imageURL);
+                    animalImage.attr("data-state" , "still");
+                    animalImage.addClass("animateThatBitch");
+                    animalDiv.append(animalImage);
+                    $("#cute-animals").append(animalDiv);
+                }
+        });
+    });
+};
+
+displayCuteAnimals();
+
+$(document).on("click" , "img.animateThatBitch" , function() {
+    console.log(this)
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src" , $(this).attr("data-animate"));
+        $(this).attr("data-state" , "animate");
+    } else {
+        $(this).attr("src" , $(this).attr("data-still"));
+        $(this).attr("data-state" , "still");
+    }
+}); 
